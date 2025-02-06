327c9aff22fed68479c5241102d309bd2b4140db
The SwiftCoder <techbymdwasirul2024@gmail.com>
2025-02-07 00:16:52 +0530

    Hooks Introduces as superPower of React to update DOM

--- episode4/src/components/Body.js
+++ episode4/src/components/Body.js
@@ -1,12 +1,87 @@
 import RestaurantCard from "./RestaurantCard";
 import resObjLIst from "../utils/mockData";
 
- export const Body = () => {
+// Now we use State Variable-->SuperPower Of React ---> We Use HookS -->useState();
+
+// niormal javascript variable
+let listOfRestaurants = [
+  {
+    info: {
+      id: "10492",
+      name: "Pizza Hut",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/16/ef7ea2a1-7657-4a33-8f15-ad2521674114_10492.jpg",
+
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 3.4,
+      parentId: "721",
+      avgRatingString: "3.8",
+      totalRatingsString: "29K+",
+      sla: {
+        deliveryTime: 48,
+      },
+    },
+  },
+  {
+    info: {
+      id: "10493",
+      name: "MAC DONALD'S",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/16/ef7ea2a1-7657-4a33-8f15-ad2521674114_10492.jpg",
+
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 3.1,
+      parentId: "721",
+      avgRatingString: "4.2",
+      totalRatingsString: "29K+",
+      sla: {
+        deliveryTime: 48,
+      },
+    },
+  },
+  {
+    info: {
+      id: "10494",
+      name: "MCD",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/16/ef7ea2a1-7657-4a33-8f15-ad2521674114_10492.jpg",
+
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.1,
+      parentId: "721",
+      avgRatingString: "4.1",
+      totalRatingsString: "29K+",
+      sla: {
+        deliveryTime: 48,
+      },
+    },
+  },
+];
+
+const Body = () => {
   return (
     <div className="body">
-      <div className="search">Search</div>
+      {/* <div className="search">Search</div> */}
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            //filter logic here
+            // console.log("btn clicked ");
+            listOfRestaurants = listOfRestaurants.filter(
+              (res) => res.info.avgRatingString > 4
+            );
+            console.log(listOfRestaurants);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resObjLIst.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
