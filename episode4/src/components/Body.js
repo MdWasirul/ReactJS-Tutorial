635d73ac664708c379729efce3e172099ebe4532
The SwiftCoder <techbymdwasirul2024@gmail.com>
2025-02-11 23:54:53 +0530

    Dynamic ui  using useState and useEffect

--- episode4/src/components/Body.js
+++ episode4/src/components/Body.js
@@ -1,10 +1,13 @@
 import RestaurantCard from "./RestaurantCard";
 import resObjLIst from "../utils/mockData";
 import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  // Now we use State Variable-->SuperPower Of React ---> We Use HookS -->useState(); 
+  // Now we use State Variable-->SuperPower Of React ---> We Use HookS -->useState();
   const [ListOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurantList, setFiltereRestaurantList] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     // console.log("UseEffect is Called! ");
@@ -13,36 +16,64 @@ const Body = () => {
 
   const fetchData = async () => {
     const response = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4554218&lng=78.39488759999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4486497&lng=78.3907546&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const jsonData = await response.json();
-    console.log(jsonData);
+    // console.log(jsonData);
     setListOfRestaurants(
-      jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+    setFiltereRestaurantList(
+      jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
     );
   };
-
-  if(ListOfRestaurants.length ==0){
-    return <h1>Loading........</h1>
-  }
-  return (
+  return ListOfRestaurants.length == 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       {/* <div className="search">Search</div> */}
       <div className="filter">
+        <div className="search">
+          <input
+            className="search-box"
+            type="text"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="search-btn"
+            onClick={() => {
+              //filter the restaurantsCards and updates the UI
+              //searchText
+              console.log(searchText);
+              const filteredRestuarant = ListOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+
+              setFiltereRestaurantList(filteredRestuarant);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
             const filteredList = ListOfRestaurants.filter(
               (res) => res.info.avgRating > 4.2
             );
-            setListOfRestaurants(filteredList);
+            setFiltereRestaurantList(filteredList);
           }}
         >
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {ListOfRestaurants.map((restaurant) => (
+        {filteredRestaurantList.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
@@ -50,3 +81,8 @@ const Body = () => {
   );
 };
 export default Body;
+
+//Conditional Rendering.
+// if (ListOfRestaurants.length == 0) {
+//   return <Shimmer />;
+// }
